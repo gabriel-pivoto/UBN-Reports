37d0d2fca128e6d6c74521939faaf7913f5a828f
Gabriel Pivoto <gabrielspivoto@gmail.com>
2023-06-03 17:48:54 -0300

    add da logica de get e post do banco de dados

--- javascript/maps.js
+++ javascript/maps.js
@@ -1,9 +1,18 @@
 
-      /**
-       * @license
-       * Copyright 2019 Google LLC. All Rights Reserved.
-       * SPDX-License-Identifier: Apache-2.0
-       */
+const firebaseConfig = {
+         apiKey: "AIzaSyAubbk47k7Zd9KDMJWCxe3X8MaNfblrS4o",
+         authDomain: "fetin-teste.firebaseapp.com",
+         databaseURL: "https://fetin-teste-default-rtdb.firebaseio.com",
+         projectId: "fetin-teste",
+         storageBucket: "fetin-teste.appspot.com",
+         messagingSenderId: "88230584865",
+         appId: "1:88230584865:web:46ee3be503d24f603bb960"
+};
+
+firebase.initializeApp(firebaseConfig);
+database = firebase.database();
+const db = firebase.firestore();
+
       let map;
       let marker;
       let geocoder;
@@ -19,21 +28,19 @@
         geocoder = new google.maps.Geocoder();
 
         const inputText = document.createElement("input");
-
         inputText.type = "text";
         inputText.placeholder = "Enter a location";
 
         const submitButton = document.createElement("input");
-
         submitButton.type = "button";
         submitButton.value = "Geocode";
         submitButton.classList.add("button", "button-primary");
 
         const clearButton = document.createElement("input");
-
         clearButton.type = "button";
         clearButton.value = "Clear";
         clearButton.classList.add("button", "button-secondary");
+
         response = document.createElement("pre");
         response.id = "response";
         response.innerText = "";
@@ -42,29 +49,32 @@
         responseDiv.appendChild(response);
 
         const instructionsElement = document.createElement("p");
-
         instructionsElement.id = "instructions";
         instructionsElement.innerHTML =
           "<strong>Instructions</strong>: Enter an address in the textbox to geocode or click on the map to reverse geocode.";
+
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputText);
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(submitButton);
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(clearButton);
-        map.controls[google.maps.ControlPosition.LEFT_TOP].push(
-          instructionsElement
-        );
+        map.controls[google.maps.ControlPosition.LEFT_TOP].push(instructionsElement);
         map.controls[google.maps.ControlPosition.LEFT_TOP].push(responseDiv);
+
         marker = new google.maps.Marker({
           map,
         });
+
         map.addListener("click", (e) => {
           geocode({ location: e.latLng });
         });
+
         submitButton.addEventListener("click", () =>
           geocode({ address: inputText.value })
         );
+
         clearButton.addEventListener("click", () => {
           clear();
         });
+
         clear();
       }
 
@@ -78,16 +88,48 @@
           .geocode(request)
           .then((result) => {
             const { results } = result;
-
+      
             map.setCenter(results[0].geometry.location);
             marker.setPosition(results[0].geometry.location);
             marker.setMap(map);
             response.innerText = JSON.stringify(result, null, 2);
+      
+            // Armazena os dados no Firebase Realtime Database
+            const data = {
+                address: results[0].formatted_address,
+                location: {
+                  lat: results[0].geometry.location.lat(),
+                  lng: results[0].geometry.location.lng(),
+                },
+              };
+            
+              db.collection("ocorrencias").add(data)
+                .then(() => {
+                  console.log("Dados salvos no Cloud Firestore");
+                })
+                .catch((error) => {
+                  console.error("Erro ao salvar os dados no Cloud Firestore:", error);
+                });
             return results;
-          })
-          .catch((e) => {
-            alert("Geocode was not successful for the following reason: " + e);
           });
+          
+          const occurrencesRef = db.collection("ocorrencias");
+
+  occurrencesRef
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        if (data.location) {
+          const { lat, lng } = data.location;
+          console.log("Latitude:", lat);
+          console.log("Longitude:", lng);
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar os dados do Cloud Firestore:", error);
+    });
       }
 
-      window.initMap = initMap;
+      
