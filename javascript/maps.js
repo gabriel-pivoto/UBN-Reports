561be845e84cba182f3f2042d88a4c52d5d68893
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2023-07-17 20:11:59 -0300

    add tela de infowindow

--- javascript/maps.js
+++ javascript/maps.js
@@ -86,6 +86,7 @@ function initMap() {
     }
   });
 
+
   submitButton.addEventListener("click", () =>
     geocode({ address: inputText.value })
   );
@@ -134,14 +135,42 @@ function Confirm() {
   modal.close();
 }
 
-
-
-
-function PegarUmaOcorrencia(id){
-  requisicao.open("GET", "http://localhost:5000/pegar/ocorrencia/"+id)
+function PegarUmaOcorrencia(id) {
+  requisicao.open("GET", "http://localhost:5000/pegar/ocorrencia/" + id)
   requisicao.setRequestHeader("Content-type", "application/json", "Access-Control-Allow-Origin")
   requisicao.onload = function teste() {
-      console.log(requisicao.response)
+    let ocorrencia = JSON.parse(requisicao.response)
+
+    let position = new google.maps.LatLng(ocorrencia.latitude, ocorrencia.longitude)
+    contentString =
+      '<div id="content">' +
+      '<div id="siteNotice">' +
+      "</div>" +
+      '<h1 id="firstHeading" class="firstHeading">'+ocorrencia.ocorrencia+'</h1>' +
+      '<div id="bodyContent">' +
+      "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+      "sandstone rock formation in the southern part of the " +
+      "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+      "south west of the nearest large town, Alice Springs; 450&#160;km " +
+      "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+      "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+      "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+      "Aboriginal people of the area. It has many springs, waterholes, " +
+      "rock caves and ancient paintings. Uluru is listed as a World " +
+      "Heritage Site.</p>" +
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+      "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+      "(last visited June 22, 2009).</p>" +
+      "</div>" +
+      "</div>";
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      position: position,
+      ariaLabel: ocorrencia.ocorrencia,
+    });
+    infowindow.open({
+      map
+    })
   }
   requisicao.send();
 }
@@ -182,7 +211,7 @@ function pegar() {
 
         let posicao = new google.maps.LatLng(latitude, longitude);
 
-        
+
 
 
         marcador(endereco, posicao, ocorrencia, key);
@@ -196,10 +225,6 @@ function pegar() {
   requisicao.send();
 }
 function marcador(endereco, posicao, ocorrencia, id) {
-
-  
-  
-
   markers = new google.maps.Marker({
     position: posicao,
     map: map,
@@ -208,8 +233,10 @@ function marcador(endereco, posicao, ocorrencia, id) {
 
   })
   markers.addListener("click", () => {
-    PegarUmaOcorrencia(id);
-    });
+    PegarUmaOcorrencia(id,);
+
+
+  });
 
 }
 
